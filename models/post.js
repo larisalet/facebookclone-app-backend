2fe12d5e7164992879556bde7c7b2f2d88e1e2b3
Let Larisa <larisalet@yahoo.com>
2021-06-22 22:18:22 +0300

    edit post

--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
     id: {
-        type: int,
+        type: Number,
         required: true,
     },
     avatar: {
